146f3a31e9611b032a9e4cdb6abc3de232dd5800
Nick Quebbeman <n.quebb@gmail.com>
2023-06-07 21:22:58 -0700

    Update transformDMMF.ts

--- src/generator/transformDMMF.ts
+++ src/generator/transformDMMF.ts
@@ -3,23 +3,21 @@ import type { DMMF } from '@prisma/generator-helper';
 const transformField = (field: DMMF.Field) => {
   const tokens = [field.name + ':'];
   let inputTokens = [];
-  const deps = new Set();
 
   if (['Int', 'Float', 'Decimal'].includes(field.type)) {
-    tokens.push('Type.Number()');
+    tokens.push('t.Number()');
   } else if (['BigInt'].includes(field.type)) {
-    tokens.push('Type.Integer()');
+    tokens.push('t.Integer()');
   } else if (['String', 'DateTime', 'Json', 'Date'].includes(field.type)) {
-    tokens.push('Type.String()');
+    tokens.push('t.String()');
   } else if (field.type === 'Boolean') {
-    tokens.push('Type.Boolean()');
+    tokens.push('t.Boolean()');
   } else {
     tokens.push(`::${field.type}::`);
-    deps.add(field.type);
   }
 
   if (field.isList) {
-    tokens.splice(1, 0, 'Type.Array(');
+    tokens.splice(1, 0, 't.Array(');
     tokens.splice(tokens.length, 0, ')');
   }
 
@@ -27,39 +25,33 @@ const transformField = (field: DMMF.Field) => {
 
   // @id cannot be optional except for input if it's auto increment
   if (field.isId && (field?.default as any)?.name === 'autoincrement') {
-    inputTokens.splice(1, 0, 'Type.Optional(');
+    inputTokens.splice(1, 0, 't.Optional(');
     inputTokens.splice(inputTokens.length, 0, ')');
   }
 
   if ((!field.isRequired || field.hasDefaultValue) && !field.isId) {
-    tokens.splice(1, 0, 'Type.Optional(');
+    tokens.splice(1, 0, 't.Optional(');
     tokens.splice(tokens.length, 0, ')');
-    inputTokens.splice(1, 0, 'Type.Optional(');
+    inputTokens.splice(1, 0, 't.Optional(');
     inputTokens.splice(inputTokens.length, 0, ')');
   }
 
   return {
     str: tokens.join(' ').concat('\n'),
     strInput: inputTokens.join(' ').concat('\n'),
-    deps,
   };
 };
 
 const transformFields = (fields: DMMF.Field[]) => {
-  let dependencies = new Set();
   const _fields: string[] = [];
   const _inputFields: string[] = [];
 
   fields.map(transformField).forEach((field) => {
     _fields.push(field.str);
     _inputFields.push(field.strInput);
-    [...field.deps].forEach((d) => {
-      dependencies.add(d);
-    });
   });
 
   return {
-    dependencies,
     rawString: _fields.filter((f) => !!f).join(','),
     rawInputString: _inputFields.filter((f) => !!f).join(','),
   };
@@ -90,43 +82,31 @@ const transformModel = (model: DMMF.Model, models?: DMMF.Model[]) => {
   return {
     raw,
     inputRaw,
-    deps: fields.dependencies,
   };
 };
 
 export const transformEnum = (enm: DMMF.DatamodelEnum) => {
   const values = enm.values
-    .map((v) => `${v.name}: Type.Literal('${v.name}'),\n`)
+    .map((v) => `${v.name}: t.Literal('${v.name}'),\n`)
     .join('');
 
   return [
     `export const ${enm.name}Const = {`,
     values,
     '}\n',
-    `export const ${enm.name} = Type.KeyOf(Type.Object(${enm.name}Const))\n`,
-    `export type ${enm.name}Type = Static<typeof ${enm.name}>`,
+    `export const ${enm.name} = t.KeyOf(Type.Object(${enm.name}Const))\n`,
   ].join('\n');
 };
 
 export function transformDMMF(dmmf: DMMF.Document) {
   const { models, enums } = dmmf.datamodel;
   const importStatements = new Set([
-    'import {Type, Static} from "@sinclair/typebox"',
+    'import { t } from "elysia"',
   ]);
 
   return [
     ...models.map((model) => {
-      let { raw, inputRaw, deps } = transformModel(model);
-
-      [...deps].forEach((d) => {
-        const depsModel = models.find((m) => m.name === d) as DMMF.Model;
-        if (depsModel) {
-          const replacer = transformModel(depsModel, models);
-          const re = new RegExp(`::${d}::`, 'gm');
-          raw = raw.replace(re, replacer.raw);
-          inputRaw = inputRaw.replace(re, replacer.inputRaw);
-        }
-      });
+      let { raw, inputRaw } = transformModel(model);
 
       enums.forEach((enm) => {
         const re = new RegExp(`::${enm.name}::`, 'gm');
@@ -156,7 +136,7 @@ export function transformDMMF(dmmf: DMMF.Document) {
         name: enm.name,
         inputRawString: null,
         rawString:
-          'import {Type, Static} from "@sinclair/typebox"\n\n' +
+          'import { t } from "elysia"\n\n' +
           transformEnum(enm),
       };
     }),
